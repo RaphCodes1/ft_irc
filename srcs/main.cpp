c63228d3591dbf2c845b981b9171267f294e2822
Raphael Creer <rcreer@lab3r7s7.42abudhabi.ae>
2026-01-06 18:40:10 +0400

    added ac and av

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,19 +1,31 @@
 #include "ircserv.hpp"
 #include "utils.hpp"
-int main()
-{
-    Server server;
-    std::cout << "----SERVER----" << std::endl;
-    try{
-        signal(SIGINT, Server::SignalHandler);
-        signal(SIGQUIT, Server::SignalHandler);
-        server.ServerInit();
+int main(int ac, char **av)
+{   
+    if(ac == 3)
+    {   
+        std::cout << "User Input: ";
+        for(size_t i = 1; i < 3; i++)
+            std::cout << av[i] << " ";
+        std::cout << std::endl;
+
+        Server server;
+        std::cout << "----SERVER----" << std::endl;
+        try{
+            signal(SIGINT, Server::SignalHandler);
+            signal(SIGQUIT, Server::SignalHandler);
+            server.ServerInit();
+        }
+        catch(const std::exception& e){
+            server.CloseFds();
+            std::cerr << e.what() << std::endl;
+            return 1;
+        }
+        std::cout << "The Server is closed!" << std::endl;
+        return 0;
     }
-    catch(const std::exception& e){
-        server.CloseFds();
-        std::cerr << e.what() << std::endl;
-        return 1;
+    else
+    {
+        std::cout << RED << "Invalid Arguments!" << std::endl;
     }
-    std::cout << "The Server is closed!" << std::endl;
-    return 0;
 }
